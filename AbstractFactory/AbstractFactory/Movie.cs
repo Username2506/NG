f5614ed2b7ca53cc00f96825f4c4b431ae1b37be
username2506 <jjpp1605@gmail.com>
2021-02-12 00:44:59 +0200

    added abstract factory pattern example

---
+++ AbstractFactory/AbstractFactory/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory
+{
+    class Movie
+    {
+        ISound sound;
+        ISubtitles subtitles;
+
+        public Movie(ILanguage lang)
+        {
+            sound = lang.sound();
+            subtitles = lang.subtitles();
+        }
+
+        public void setLanguage(ILanguage lang)
+        {
+            sound = lang.sound();
+            subtitles = lang.subtitles();
+        }
+
+        public void showMovie()
+        {
+            sound.playSound();
+            subtitles.showSubtitles();
+        }
+    }
+}
---
+++ AbstractFactory/AbstractFactory/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie = new Movie(new EnLanguage());
+
+            movie.showMovie();
+        }
+    }
+}
